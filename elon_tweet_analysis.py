771243a5fb1875137d1beffbe2b9ab436dd0d4c5
tyler <n.tyler975@gmail.com>
2025-03-29 18:11:12 -0700

    modified csv path

--- elon_tweet_analysis.py
+++ elon_tweet_analysis.py
@@ -8,7 +8,7 @@ with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 # Use the path from the config file
-file_path = config["data_path"]
+file_path = config["tweets_data_path"]
 
 # Read the CSV using the file path from the config
 df = pd.read_csv(file_path, low_memory=False)
