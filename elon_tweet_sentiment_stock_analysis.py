44c1021f205c328fa5a750e5f11ee5e0079635a5
tyler <n.tyler975@gmail.com>
2025-03-29 17:14:47 -0700

    implement VADER & append sentiment scores to each tweet

--- elon_tweet_sentiment_stock_analysis.py
+++ elon_tweet_sentiment_stock_analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import yaml
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # Load the configuration file
 with open("config.yaml", "r") as file:
@@ -9,7 +11,24 @@ with open("config.yaml", "r") as file:
 file_path = config["data_path"]
 
 # Read the CSV using the file path from the config
-df = pd.read_csv(file_path)
+df = pd.read_csv(file_path, low_memory=False)
 
-# Print the first few rows of the dataframe
-print(df.head())
+# # Download VADER lexicon (if first time using it)
+# nltk.download("vader_lexicon")
+
+# Initialize the SentimentIntensityAnalyzer
+sia = SentimentIntensityAnalyzer()
+
+# Function to calculate sentiment score for a tweet
+def get_sentiment_score(tweet):
+    sentiment = sia.polarity_scores(tweet)
+    return sentiment["compound"]  # Compound score is a summary metric
+
+# Apply sentiment analysis to the tweet column and create a new column
+df["sentiment_score"] = df["fullText"].apply(get_sentiment_score)
+
+# Sort the DataFrame by 'sentiment_score' in descending order and print the top 5
+top_5_sentiments = df.sort_values(by='sentiment_score', ascending=False).head(5)
+
+# Print the top 5 rows
+print(top_5_sentiments)
